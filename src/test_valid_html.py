c5b3b102ce783bd4530b1c8d09470d57f7756bc6
anaguilar02 <isaagui2002@gmail.com>
2024-03-28 17:07:10 -0600

    src
    Signed-off-by: anaguilar02 <isaagui2002@gmail.com>

---
+++ src/test_valid_html.py
@@ -0,0 +1,27 @@
+# usage: python3 -m unittest -v test_valid_in
+
+import jflap_cnf_parser as jcp
+'''
+@pytest.mark.parametrize("input, accepted", [
+    ("(ab?c)*","<class 'finalwa.FA'>"),
+    ("(^a)*(b(hola(hi)+hel*lo)d(012?3)+)*glue?(adios*)$", "<class 'finalwa.FA'>"),
+    ("(a*(bd(012?3)+)*glue)", "<class 'finalwa.FA'>"),
+    (")(j)", "<class 'NoneType'>"),
+    ("@hola*(adios)", "<class 'NoneType'>"),
+    ("x+", "<class 'finalwa.FA'>"),
+    ("((no)", "<class 'NoneType'>"),
+    ("((^a)*(b(hola(hi)+hel*lo)d(012?3)+)*glue?(adios*)$)*", "<class 'finalwa.FA'>"),
+    
+    ])  '''
+def test_valid_word():
+    # tuve que quitar static de load regex y hacerlo function en vez de method
+    #assert (str(type(fw.load_regex(input))) == str(accepted))
+    
+    # Assert that the connection is added correctly
+    assert(jcp.word_accept("<html><head> <title> z </title> </head><body> <br> z </br> </body></html>".lower().split()) > 0)
+    assert(jcp.word_accept("<html><head> <title> </title> </head><body> <br> </br> </body></html>".lower().split()) > 0)
+    assert(jcp.word_accept("<html><head> <title> z </title> </head><body> </body></html>".lower().split()) > 0)
+    assert(jcp.word_accept("<html><head> <title> z </title> </head><body> <br>  </br> z </body></html>".lower().split()) > 0)
+    assert(jcp.word_accept("<html><head> <title> z </title> </head><body> <br> z </br> <hr> z </hr> <br> z </br> </body></html>".lower().split()) > 0)
+    assert(jcp.word_accept("<html><head> <title> z </title> </head><body> <br> z </br> <hr> z </hr> <br> </br> z </body></html>".lower().split()) > 0)
+    assert(jcp.word_accept("<html><head> <title> z </title> </head><body> z z </body></html>".lower().split()) > 0)
