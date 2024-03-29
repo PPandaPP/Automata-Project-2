75d4e4605b0d93c708fefca893defae43ee41b1e
anaguilar02 <isaagui2002@gmail.com>
2024-03-29 11:02:02 -0600

    added script to remove all epsilon rules (step 1 and 2 of CNF conversion)
    Signed-off-by: anaguilar02 <isaagui2002@gmail.com>

--- src/jflap_cnf_parser.py
+++ src/jflap_cnf_parser.py
@@ -1,6 +1,6 @@
 # parser for CFG in .jff to CNF
 from nltk import CFG, Nonterminal, nonterminals, Production, RecursiveDescentParser
-
+import eps
 from io import StringIO
 import xml.etree.ElementTree as ET
 
@@ -24,65 +24,52 @@ def CFG_to_string():
                 
             if(c2.tag == "right"):
                 temp = str(c2.text).split("\"")
- 
+
                 for i in range(len(temp)):
-                
                     if(len(temp[i]) > 0 and not temp[i].isupper()):
+                        
                         temp_rule.append("L"+ str(counter_new_rules)) 
-                        
                         # create new rule with just one terminal
-                        if(str(temp[i]) == 'None'):
-                            temp[i] = ''
-                        temp_gg += "L"+ str(counter_new_rules) + " -> " + "'" + str(temp[i]) + "'" + "\n"
-                        
+                        temp_gg += "L"+ str(counter_new_rules) + " -> " + "'" + temp[i] + "'" + "\n"
                         # counts amount of new rules created
                         counter_new_rules += 1
                     else:
                         temp_rule.append(temp[i])
-
-                gg += str(temp_rule[0]) + " -> "
-                for i in range(1, len(temp_rule)):
-                    gg += str(temp_rule[i]) + " "
-                gg += "\n"
+                # check invalid
+                gg += temp_rule[0] + " -> " + " ".join(temp_rule[1:]) + "\n"
                 gg += temp_gg
                 
                 temp_gg = ""
-                temp_rule = []
-                
+                temp_rule = []            
     return(gg)
+
+    
 
 def word_accept(sentence): 
     g0 = CFG_to_string()
-    #print(g0)
+    print(g0)
     g1 = CFG.fromstring(g0)
-    #c = g1.chomsky_normal_form() 
-    #print("Printing productions in CNF...")
-    for p in g1.productions():           
-        print(p)  
-        
-    rd = RecursiveDescentParser(g1)  
     
-    print("Recursion....", sentence)
+    eps.remove_all_epsilons(g1)
+    
+    for production in g1.productions():
+        print(production)
+    
+    c = g1.chomsky_normal_form() # not finishing
+    #print("Printing productions in CNF...")
+    #for p in g1.productions():           
+    #    print(p)  
+        
+    '''rd = RecursiveDescentParser(g1)  
+    
+    print("Recursion....")
     count_steps = 0
     for t in rd.parse(sentence.lower().split()):
         print(t)
-        count_steps+=1
-    
-    return(count_steps)
-
-    
+        count_steps+=1'''
+        
+    #return(count_steps)
 
 
-#sentence = "<html><head> <title> z </title> </head><body> <br> z </br> </body></html>".lower().split()
+word_accept("<html><head> <title> </title> </head><body> z </body></html>")
 
-
-
-# transform to CNF
-#print("Transforming CFG to CNF")
-# chomsky_normal_form() doesnt deal with empty
-#c = g1.chomsky_normal_form() 
-            
-print("Printing productions in CNF...")
-#for p in c.productions():           
-    ##print(p)   
-    
